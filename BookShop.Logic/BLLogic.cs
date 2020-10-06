467e4ef221fe9ddf75ff8f2bda2ed3efbf2a7615
adriandrotar <72419741+adriandrotar@users.noreply.github.com>
2020-10-06 02:50:34 +0200

    Add files via upload

---
+++ BookShop.Logic/BLLogic.cs
@@ -0,0 +1,357 @@
+﻿// <copyright file="BLLogic.cs" company="BCXFMD">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace BookShop.Logic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Net;
+    using BookShop.Repository;
+
+    /// <summary>
+    /// A class which contains the BL methods(CRUD + NON CRUD).
+    /// </summary>
+    public class BLLogic : ILogic
+    {
+        private readonly RepositoryCollection repositoryCollection;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BLLogic"/> class.
+        /// </summary>
+        public BLLogic()
+        {
+            this.repositoryCollection = new RepositoryCollection();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BLLogic"/> class. For testing.
+        /// </summary>
+        /// <param name="inj">injected repocollection.</param>
+        public BLLogic(RepositoryCollection inj)
+        {
+            this.repositoryCollection = inj;
+        }
+
+        /// <inheritdoc/>
+        public string AuthorContents
+        {
+            get { return this.repositoryCollection.AuthorRepository.TableContents; }
+        }
+
+        /// <inheritdoc/>
+        public string BookContents
+        {
+            get { return this.repositoryCollection.BookRepository.TableContents; }
+        }
+
+        /// <inheritdoc/>
+        public string CustomerContents
+        {
+            get { return this.repositoryCollection.CustomerRepository.TableContents; }
+        }
+
+        /// <inheritdoc/>
+        public string GenreContents
+        {
+            get { return this.repositoryCollection.GenreRepository.TableContents; }
+        }
+
+        /// <inheritdoc/>
+        public string OrdersContents
+        {
+            get { return this.repositoryCollection.OrdersRepository.TableContents; }
+        }
+
+        /// <inheritdoc/>
+        public string PublisherContents
+        {
+            get { return this.repositoryCollection.PublisherRepository.TableContents; }
+        }
+
+        /// <inheritdoc/>
+        public string GetAllContents
+        {
+            get { return this.repositoryCollection.GetAllContents(); }
+        }
+
+        /// <inheritdoc/>
+        public bool AddAuthor(int authorid, string name, string country)
+        {
+            return this.repositoryCollection.AuthorRepository.Add(authorid, name, country);
+        }
+
+        /// <inheritdoc/>
+        public bool AddBook(int bookid, string title, int authorid, int publisherid, int genreid, string description, int rating, int price)
+        {
+            return this.repositoryCollection.BookRepository.Add(bookid, title, authorid, publisherid, genreid, description, rating, price);
+        }
+
+        /// <inheritdoc/>
+        public bool AddCustomer(int customerid, string name, string city, string address, string phone, string email)
+        {
+            return this.repositoryCollection.CustomerRepository.Add(customerid, name, city, address, phone, email);
+        }
+
+        /// <inheritdoc/>
+        public bool AddGenre(int genreid, string name, string genredescription)
+        {
+            return this.repositoryCollection.GenreRepository.Add(genreid, name, genredescription);
+        }
+
+        /// <inheritdoc/>
+        public bool AddOrders(int ordersid, int customerid, string shop, int bookid, string purchasedate, string comments)
+        {
+            return this.repositoryCollection.OrdersRepository.Add(ordersid, customerid, shop, bookid, purchasedate, comments);
+        }
+
+        /// <inheritdoc/>
+        public bool AddPublisher(int publisherid, string name, string country)
+        {
+            return this.repositoryCollection.PublisherRepository.Add(publisherid, name, country);
+        }
+
+        /// <inheritdoc/>
+        public void RemoveAuthor(int id)
+        {
+            this.repositoryCollection.AuthorRepository.Remove(id);
+        }
+
+        /// <inheritdoc/>
+        public void RemoveBook(int id)
+        {
+            this.repositoryCollection.BookRepository.Remove(id);
+        }
+
+        /// <inheritdoc/>
+        public void RemoveCustomer(int id)
+        {
+            this.repositoryCollection.CustomerRepository.Remove(id);
+        }
+
+        /// <inheritdoc/>
+        public void RemoveGenre(int id)
+        {
+            this.repositoryCollection.GenreRepository.Remove(id);
+        }
+
+        /// <inheritdoc/>
+        public void RemoveOrders(int id)
+        {
+            this.repositoryCollection.OrdersRepository.Remove(id);
+        }
+
+        /// <inheritdoc/>
+        public void RemovePublisher(int id)
+        {
+            this.repositoryCollection.PublisherRepository.Remove(id);
+        }
+
+        /// <inheritdoc/>
+        public bool UpdateAuthor(int authorid, string name, string country)
+        {
+            return this.repositoryCollection.AuthorRepository.Update(authorid, name, country);
+        }
+
+        /// <inheritdoc/>
+        public bool UpdateBook(int bookid, string title, int authorid, int publisherid, int genreid, string description, int rating, int price)
+        {
+            return this.repositoryCollection.BookRepository.Update(bookid, title, authorid, publisherid, genreid, description, rating, price);
+        }
+
+        /// <inheritdoc/>
+        public bool UpdateCustomer(int customerid, string name, string city, string address, string phone, string email)
+        {
+            return this.repositoryCollection.CustomerRepository.Update(customerid, name, city, address, phone, email);
+        }
+
+        /// <inheritdoc/>
+        public bool UpdateGenre(int genreid, string name, string genredescription)
+        {
+            return this.repositoryCollection.GenreRepository.Update(genreid, name, genredescription);
+        }
+
+        /// <inheritdoc/>
+        public bool UpdateOrders(int ordersid, int customerid, string shop, int bookid, string purchasedate, string comments)
+        {
+            return this.repositoryCollection.OrdersRepository.Update(ordersid, customerid, shop, bookid, purchasedate, comments);
+        }
+
+        /// <inheritdoc/>
+        public bool UpdatePublisher(int publisherid, string name, string country)
+        {
+            return this.repositoryCollection.PublisherRepository.Update(publisherid, name, country);
+        }
+
+        /// <inheritdoc/>
+        public string GetAuthorById(int id)
+        {
+            return this.repositoryCollection.AuthorRepository.GetAuthorById(id);
+        }
+
+        /// <inheritdoc/>
+        public string GetBookById(int id)
+        {
+            return this.repositoryCollection.BookRepository.GetBookById(id);
+        }
+
+        /// <inheritdoc/>
+        public string GetCustomerById(int id)
+        {
+            return this.repositoryCollection.CustomerRepository.GetCustomerById(id);
+        }
+
+        /// <inheritdoc/>
+        public string GetGenreById(int id)
+        {
+            return this.repositoryCollection.GenreRepository.GetGenreById(id);
+        }
+
+        /// <inheritdoc/>
+        public string GetOrdersById(int id)
+        {
+            return this.repositoryCollection.OrdersRepository.GetOrdersById(id);
+        }
+
+        /// <inheritdoc/>
+        public string GetPublisherById(int id)
+        {
+            return this.repositoryCollection.PublisherRepository.GetPublisherById(id);
+        }
+
+        /// <inheritdoc/>
+        public bool AuthorExists(int id)
+        {
+            return this.repositoryCollection.AuthorRepository.IsExists(id);
+        }
+
+        /// <inheritdoc/>
+        public bool BookExists(int id)
+        {
+            return this.repositoryCollection.BookRepository.IsExists(id);
+        }
+
+        /// <inheritdoc/>
+        public bool CustomerExists(int id)
+        {
+            return this.repositoryCollection.CustomerRepository.IsExists(id);
+        }
+
+        /// <inheritdoc/>
+        public bool GenreExists(int id)
+        {
+            return this.repositoryCollection.GenreRepository.IsExists(id);
+        }
+
+        /// <inheritdoc/>
+        public bool OrdersExists(int id)
+        {
+            return this.repositoryCollection.OrdersRepository.IsExists(id);
+        }
+
+        /// <inheritdoc/>
+        public bool PublisherExists(int id)
+        {
+            return this.repositoryCollection.PublisherRepository.IsExists(id);
+        }
+
+        /// <inheritdoc/>
+        public List<string> BooksSoldInShop(string shop)
+        {
+            List<string> bookslist = new List<string>();
+
+            var answer = from x in this.repositoryCollection.OrdersRepository.All
+                         join y in this.repositoryCollection.BookRepository.All on x.BOOKID equals y.BOOKID
+                         where x.SHOP == shop
+                         select new
+                         {
+                             BookName = y.TITLE,
+                         };
+            foreach (var book in answer)
+            {
+                if (!bookslist.Contains(book.BookName))
+                {
+                    bookslist.Add(book.BookName);
+                }
+            }
+
+            return bookslist;
+        }
+
+        /// <inheritdoc/>
+        public List<string> AuthorsAndBooksExpensive()
+        {
+            List<string> authorbookcollection = new List<string>();
+
+            var answer = from x in this.repositoryCollection.AuthorRepository.All
+                         join y in this.repositoryCollection.BookRepository.All on x.AUTHORID equals y.AUTHORID
+                         where y.PRICE > 3000
+                         orderby y.PRICE
+                         select new
+                         {
+                             AuthorBookPrice = x.NAME + " : " + y.TITLE + " : " + y.PRICE,
+                         };
+
+            foreach (var info in answer)
+            {
+                if (!authorbookcollection.Contains(info.AuthorBookPrice))
+                {
+                    authorbookcollection.Add(info.AuthorBookPrice);
+                }
+            }
+
+            return authorbookcollection;
+        }
+
+        /// <inheritdoc/>
+        public List<string> GenresAndBooksRatingMedium()
+        {
+            List<string> genresbookrating = new List<string>();
+
+            var answer = from x in this.repositoryCollection.GenreRepository.All
+                         join y in this.repositoryCollection.BookRepository.All on x.GENREID equals y.GENREID
+                         where y.RATING > 5
+                         orderby y.RATING descending
+                         select new
+                         {
+                             TitleGenreRating = y.TITLE + " : " + x.NAME + " : " + y.RATING,
+                         };
+            foreach (var info in answer)
+            {
+                if (!genresbookrating.Contains(info.TitleGenreRating))
+                {
+                    genresbookrating.Add(info.TitleGenreRating);
+                }
+            }
+
+            return genresbookrating;
+        }
+
+        /// <inheritdoc/>
+        public string GetIfBestSellerOrNot(string book)
+        {
+            string bestseller = string.Empty;
+            string url = @"http://localhost:8080/BookShop/BookShopServlet?book=" + book;
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.AutomaticDecompression = DecompressionMethods.GZip;
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+                {
+                    bestseller = reader.ReadToEnd();
+                }
+            }
+            catch
+            {
+                bestseller = "Nem lehet kapcsolódni a szerverhez.";
+            }
+
+            return bestseller;
+        }
+    }
+}
---
+++ BookShop.Logic/ILogic.cs
@@ -0,0 +1,325 @@
+﻿// <copyright file="ILogic.cs" company="BCXFMD">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace BookShop.Logic
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// An interface which defines the BL methods(CRUD + NON CRUD).
+    /// </summary>
+    public interface ILogic
+    {
+        /// <summary>
+        /// Gets all the contents from the Author table.
+        /// </summary>
+        string AuthorContents { get; }
+
+        /// <summary>
+        /// Gets all the contents from the Book table.
+        /// </summary>
+        string BookContents { get; }
+
+        /// <summary>
+        /// Gets all the contents from the Customer table.
+        /// </summary>
+        string CustomerContents { get; }
+
+        /// <summary>
+        /// Gets all the contents from the Genre table.
+        /// </summary>
+        string GenreContents { get; }
+
+        /// <summary>
+        /// Gets all the contents from the Orders table.
+        /// </summary>
+        string OrdersContents { get; }
+
+        /// <summary>
+        /// Gets all the contents from the Publisher table.
+        /// </summary>
+        string PublisherContents { get; }
+
+        /// <summary>
+        /// Gets all the contents from every table.
+        /// </summary>
+        string GetAllContents { get; }
+
+        /// <summary>
+        /// Adds a new Author to the DB.
+        /// </summary>
+        /// <param name="authorid">Id of author.</param>
+        /// <param name="name">Name of author.</param>
+        /// <param name="country">Country of author.</param>
+        /// <returns>True if added.</returns>
+        bool AddAuthor(int authorid, string name, string country);
+
+        /// <summary>
+        /// Adds a new Book to the DB.
+        /// </summary>
+        /// <param name="bookid">Id of the book.</param>
+        /// <param name="title">Title of the book.</param>
+        /// <param name="authorid">Author ID of the book.</param>
+        /// <param name="publisherid">Publisher ID of the book.</param>
+        /// <param name="genreid">Genre ID of the book.</param>
+        /// <param name="description">Description of the book.</param>
+        /// <param name="rating">Rating of the book.</param>
+        /// <param name="price">Price of the book.</param>
+        /// <returns>True if added.</returns>
+        bool AddBook(int bookid, string title, int authorid, int publisherid, int genreid, string description, int rating, int price);
+
+        /// <summary>
+        /// Adds a new Customer to the DB.
+        /// </summary>
+        /// <param name="customerid">Id of the customer.</param>
+        /// <param name="name">Name of the customer.</param>
+        /// <param name="city">City of the customer.</param>
+        /// <param name="address">Address of the customer.</param>
+        /// <param name="phone">Phone of the customer.</param>
+        /// <param name="email">Email of the customer.</param>
+        /// <returns>True if added.</returns>
+        bool AddCustomer(int customerid, string name, string city, string address, string phone, string email);
+
+        /// <summary>
+        /// Adds a new Genre to the DB.
+        /// </summary>
+        /// <param name="genreid">ID of the genre.</param>
+        /// <param name="name">Name of the genre.</param>
+        /// <param name="genredescription">Description of the genre.</param>
+        /// <returns>True if added.</returns>
+        bool AddGenre(int genreid, string name, string genredescription);
+
+        /// <summary>
+        /// Adds a new Order to the DB.
+        /// </summary>
+        /// <param name="ordersid">ID of the order.</param>
+        /// <param name="customerid">Customer ID  of the order.</param>
+        /// <param name="shop">Shop of the order.</param>
+        /// <param name="bookid">Book ID of the order.</param>
+        /// <param name="purchasedate">PurchaseDate of the order.</param>
+        /// <param name="comments">Comments of the order.</param>
+        /// <returns>True if added.</returns>
+        bool AddOrders(int ordersid, int customerid, string shop, int bookid, string purchasedate, string comments);
+
+        /// <summary>
+        /// Adds a new Publisher to the DB.
+        /// </summary>
+        /// <param name="publisherid">ID of the publisher.</param>
+        /// <param name="name">Name of the publisher.</param>
+        /// <param name="country">Country of the publisher.</param>
+        /// <returns>True if added.</returns>
+        bool AddPublisher(int publisherid, string name, string country);
+
+        /// <summary>
+        /// Removes an author by id from the DB.
+        /// </summary>
+        /// <param name="id">ID of the author.</param>
+        void RemoveAuthor(int id);
+
+        /// <summary>
+        /// Removes a book by id from the DB.
+        /// </summary>
+        /// <param name="id">ID of the book.</param>
+        void RemoveBook(int id);
+
+        /// <summary>
+        /// Removes a customer by id from the DB.
+        /// </summary>
+        /// <param name="id">ID of the customer.</param>
+        void RemoveCustomer(int id);
+
+        /// <summary>
+        /// Removes a Genre by id from the DB.
+        /// </summary>
+        /// <param name="id">ID of the genre.</param>
+        void RemoveGenre(int id);
+
+        /// <summary>
+        /// Removes an order by id from the DB.
+        /// </summary>
+        /// <param name="id">ID of the order.</param>
+        void RemoveOrders(int id);
+
+        /// <summary>
+        /// Removes a publisher by id from the DB.
+        /// </summary>
+        /// <param name="id">ID of the publisher.</param>
+        void RemovePublisher(int id);
+
+        /// <summary>
+        /// Updates an Author from the DB.
+        /// </summary>
+        /// <param name="authorid">Id of author.</param>
+        /// <param name="name">Name of author.</param>
+        /// <param name="country">Country of author.</param>
+        /// <returns>True if updated.</returns>
+        bool UpdateAuthor(int authorid, string name, string country);
+
+        /// <summary>
+        /// Updates a Book from the DB.
+        /// </summary>
+        /// <param name="bookid">Id of the book.</param>
+        /// <param name="title">Title of the book.</param>
+        /// <param name="authorid">Author ID of the book.</param>
+        /// <param name="publisherid">Publisher ID of the book.</param>
+        /// <param name="genreid">Genre ID of the book.</param>
+        /// <param name="description">Description of the book.</param>
+        /// <param name="rating">Rating of the book.</param>
+        /// <param name="price">Price of the book.</param>
+        /// <returns>True if updated.</returns>
+        bool UpdateBook(int bookid, string title, int authorid, int publisherid, int genreid, string description, int rating, int price);
+
+        /// <summary>
+        /// Updates a Customer from the DB.
+        /// </summary>
+        /// <param name="customerid">Id of the customer.</param>
+        /// <param name="name">Name of the customer.</param>
+        /// <param name="city">City of the customer.</param>
+        /// <param name="address">Address of the customer.</param>
+        /// <param name="phone">Phone of the customer.</param>
+        /// <param name="email">Email of the customer.</param>
+        /// <returns>True if updated.</returns>
+        bool UpdateCustomer(int customerid, string name, string city, string address, string phone, string email);
+
+        /// <summary>
+        /// Updates a Genre from the DB.
+        /// </summary>
+        /// <param name="genreid">ID of the genre.</param>
+        /// <param name="name">Name of the genre.</param>
+        /// <param name="genredescription">Description of the genre.</param>
+        /// <returns>True if updated.</returns>
+        bool UpdateGenre(int genreid, string name, string genredescription);
+
+        /// <summary>
+        /// Updates an Order from the DB.
+        /// </summary>
+        /// <param name="ordersid">ID of the order.</param>
+        /// <param name="customerid">Customer ID  of the order.</param>
+        /// <param name="shop">Shop of the order.</param>
+        /// <param name="bookid">Book ID of the order.</param>
+        /// <param name="purchasedate">PurchaseDate of the order.</param>
+        /// <param name="comments">Comments of the order.</param>
+        /// <returns>True if updated.</returns>
+        bool UpdateOrders(int ordersid, int customerid, string shop, int bookid, string purchasedate, string comments);
+
+        /// <summary>
+        /// Updates Publisher from the DB.
+        /// </summary>
+        /// <param name="publisherid">ID of the publisher.</param>
+        /// <param name="name">Name of the publisher.</param>
+        /// <param name="country">Country of the publisher.</param>
+        /// <returns>True if updated.</returns>
+        bool UpdatePublisher(int publisherid, string name, string country);
+
+        /// <summary>
+        /// Gives back an Author from the DB.
+        /// </summary>
+        /// <param name="id">ID of the author.</param>
+        /// <returns>Returns an author.</returns>
+        string GetAuthorById(int id);
+
+        /// <summary>
+        /// Gives back a Book from the DB.
+        /// </summary>
+        /// <param name="id">ID of the book.</param>
+        /// <returns>Returns a book.</returns>
+        string GetBookById(int id);
+
+        /// <summary>
+        /// Gives back a Customer from the DB.
+        /// </summary>
+        /// <param name="id">ID of the customer.</param>
+        /// <returns>Returns a customer.</returns>
+        string GetCustomerById(int id);
+
+        /// <summary>
+        /// Gives back a Genre from the DB.
+        /// </summary>
+        /// <param name="id">ID of the genre.</param>
+        /// <returns>Returns a genre.</returns>
+        string GetGenreById(int id);
+
+        /// <summary>
+        /// Gives back an Order from the DB.
+        /// </summary>
+        /// <param name="id">ID of the order.</param>
+        /// <returns>Returns an order.</returns>
+        string GetOrdersById(int id);
+
+        /// <summary>
+        /// Gives back a Publisher from the DB.
+        /// </summary>
+        /// <param name="id">ID of the publisher.</param>
+        /// <returns>Returns a publisher.</returns>
+        string GetPublisherById(int id);
+
+        /// <summary>
+        /// Gives back if the Author exists.
+        /// </summary>
+        /// <param name="id">ID of the author.</param>
+        /// <returns>True if it exists.</returns>
+        bool AuthorExists(int id);
+
+        /// <summary>
+        /// Gives back if the Book exists.
+        /// </summary>
+        /// <param name="id">ID of the book.</param>
+        /// <returns>True if it exists.</returns>
+        bool BookExists(int id);
+
+        /// <summary>
+        /// Gives back if the Customer exists.
+        /// </summary>
+        /// <param name="id">ID of the customer.</param>
+        /// <returns>True if it exists.</returns>
+        bool CustomerExists(int id);
+
+        /// <summary>
+        /// Gives back if the Genre exists.
+        /// </summary>
+        /// <param name="id">ID of the genre.</param>
+        /// <returns>True if it exists.</returns>
+        bool GenreExists(int id);
+
+        /// <summary>
+        /// Gives back if the Order exists.
+        /// </summary>
+        /// <param name="id">ID of the order.</param>
+        /// <returns>True if it exists.</returns>
+        bool OrdersExists(int id);
+
+        /// <summary>
+        /// Gives back if the Publisher exists.
+        /// </summary>
+        /// <param name="id">ID of the publisher.</param>
+        /// <returns>True if it exists.</returns>
+        bool PublisherExists(int id);
+
+        /// <summary>
+        /// Gives back the books that were sold in a given shop.
+        /// </summary>
+        /// <param name="shop">The name of the shop.</param>
+        /// <returns>The title of the books.</returns>
+        List<string> BooksSoldInShop(string shop);
+
+        /// <summary>
+        /// Gives back the books of the authors that are priced more than 3000 in order.
+        /// </summary>
+        /// <returns>The title of the books, with their authors name and the books price.</returns>
+        List<string> AuthorsAndBooksExpensive();
+
+        /// <summary>
+        /// Gives back the books and their genres which are rated more than 5/10.
+        /// </summary>
+        /// <returns>The books and their genres, and their rating.</returns>
+        List<string> GenresAndBooksRatingMedium();
+
+        /// <summary>
+        /// Gives back if the book is a bestseller or not.
+        /// </summary>
+        /// <param name="book">the book.</param>
+        /// <returns>The bestseller books.</returns>
+        string GetIfBestSellerOrNot(string book);
+    }
+}
